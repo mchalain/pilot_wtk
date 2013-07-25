21d7a8b6c29b354765b0239234d7246e6cc5fd55
mchalain <marc.chalain@gmail.com>
2013-07-25 15:56:37 +0200

    utils: memset3é and memset16 definition

--- include/pilot_utk.h
+++ include/pilot_utk.h
@@ -8,12 +8,10 @@
 }
 
 #ifndef memset32
-inline int memset32(void *s, uint32_t ll, uint32_t l)
-MEMSET(s, ll, l)
+inline int memset32(void *s, uint32_t ll, uint32_t l);
 #endif
 #ifndef memset16
-inline int memset16(void *s, uint16_t ll, uint32_t l)
-MEMSET(s, ll, l)
+inline int memset16(void *s, uint16_t ll, uint32_t l);
 #endif
 
 #endif
--- src/pilot_utils.c
+++ src/pilot_utils.c
@@ -1,3 +1,8 @@
 #include <stdint.h>
 #include <pilot_utk.h>
 
+inline int memset32(void *s, uint32_t ll, uint32_t l)
+MEMSET(s, ll, l)
+
+inline int memset16(void *s, uint16_t ll, uint32_t l)
+MEMSET(s, ll, l)
